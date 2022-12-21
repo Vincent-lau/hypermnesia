0a671f2d362c6e8b38cfe57ff4385c8a7789e0ab
Shuntian Liu <shuntian.liu@outlook.com>
2022-12-21 17:47:19 +0000

    feat: before changing to porset

--- src/mnesia_porset.erl
+++ src/mnesia_porset.erl
@@ -23,47 +23,33 @@
 -behaviour(mnesia_backend_type).
 
 %% Initializations
--export([init_backend/0, add_aliases/1, remove_aliases/1,
-	 check_definition/4, semantics/2]).
-
--export([
-	 create_table/3, load_table/4,
-	 delete_table/2, close_table/2, sync_close_table/2,
-
-	 sender_init/4,
-	 receiver_first_message/4, receive_data/5, receive_done/4,
-
-	 index_is_consistent/3, is_index_consistent/2,
-
-	 real_suffixes/0, tmp_suffixes/0,
-
-	 info/3,
-	 fixtable/3,
-	 validate_key/6, validate_record/6,
-
-	 first/2, last/2, next/3, prev/3, slot/3,
-
-	 insert/3, update_counter/4,
-	 lookup/3,
-	 delete/3, match_delete/3,
-	 select/1, select/3, select/4, repair_continuation/2
-	]).
-
+-export([init_backend/0, add_aliases/1, remove_aliases/1, check_definition/4,
+         semantics/2]).
+-export([create_table/3, load_table/4, delete_table/2, close_table/2, sync_close_table/2,
+         sender_init/4, receiver_first_message/4, receive_data/5, receive_done/4,
+         index_is_consistent/3, is_index_consistent/2, real_suffixes/0, tmp_suffixes/0, info/3,
+         fixtable/3, validate_key/6, validate_record/6, first/2, last/2, next/3, prev/3, slot/3,
+         insert/3, update_counter/4, lookup/3, delete/3, match_delete/3, select/1, select/3,
+         select/4, repair_continuation/2]).
 -export([register/0, register/1]).
 
 -define(DEBUG, 1).
+
 -ifdef(DEBUG).
--define(DBG(DATA), io:format("~p:~p: ~p~n",[?MODULE, ?LINE, DATA])).
--define(DBG(FORMAT, ARGS), io:format("~p:~p: " ++ FORMAT,[?MODULE, ?LINE] ++ ARGS)).
+
+-define(DBG(DATA), io:format("~p:~p: ~p~n", [?MODULE, ?LINE, DATA])).
+-define(DBG(FORMAT, ARGS), io:format("~p:~p: " ++ FORMAT, [?MODULE, ?LINE] ++ ARGS)).
+
 -else.
+
 -define(DBG(DATA), ok).
 -define(DBG(FORMAT, ARGS), ok).
+
 -endif.
 
 %% types() ->
 %%     [{fs_copies, ?MODULE},
 %%      {raw_fs_copies, ?MODULE}].
-
 
 register() ->
     register(default_alias()).
@@ -79,10 +65,12 @@ register(Alias) ->
             {error, Reason}
     end.
 
-
-semantics(porset_copies, storage) -> ram_copies;
-semantics(porset_copies, types  ) -> [set, ordered_set, bag];
-semantics(porset_copies, index_types) -> [ordered];
+semantics(porset_copies, storage) ->
+    ram_copies;
+semantics(porset_copies, types) ->
+    [set, ordered_set, bag];
+semantics(porset_copies, index_types) ->
+    [ordered];
 semantics(_Alias, _) ->
     undefined.
 
@@ -94,8 +82,8 @@ init_backend() ->
     %% cheat and stuff a marker in mnesia_gvar
     mnesia_porset_admin:ensure_started().
 
-
-default_alias() -> porset_copies.
+default_alias() ->
+    porset_copies.
 
 add_aliases(_As) ->
     lager:debug("add_aliases(~p)", [_As]),
@@ -106,7 +94,6 @@ add_aliases(_As) ->
 remove_aliases(_) ->
     % TODO remove alias
     ok.
-
 
 %% Table operations
 
@@ -119,16 +106,19 @@ create_table(porset_copies, Tab, Props) when is_atom(Tab) ->
     lager:debug("~p Create: ~p(~p) ~p~n", [self(), Tab, Tid, Props]),
     mnesia_lib:set({?MODULE, Tab}, Tid),
     ok;
-create_table(_, Tag={Tab, index, {_Where, Type0}}, _Opts) ->
-    Type = case Type0 of
-	       ordered -> ordered_set;
-	       _ -> Type0
-	   end,
+create_table(_, Tag = {Tab, index, {_Where, Type0}}, _Opts) ->
+    Type =
+        case Type0 of
+            ordered ->
+                ordered_set;
+            _ ->
+                Type0
+        end,
     Tid = ets:new(Tab, [public, Type]),
     ?DBG("~p(~p) ~p~n", [Tab, Tid, Tag]),
     mnesia_lib:set({?MODULE, Tag}, Tid),
     ok;
-create_table(_, Tag={_Tab, retainer, ChkPName}, _Opts) ->
+create_table(_, Tag = {_Tab, retainer, ChkPName}, _Opts) ->
     Tid = ets:new(ChkPName, [set, public, {keypos, 2}]),
     ?DBG("~p(~p) ~p~n", [_Tab, Tid, Tag]),
     mnesia_lib:set({?MODULE, Tag}, Tid),
@@ -136,57 +126,65 @@ create_table(_, Tag={_Tab, retainer, ChkPName}, _Opts) ->
 
 delete_table(porset_copies, Tab) ->
     try
-      ets:delete(mnesia_lib:val({?MODULE,Tab})),
-      mnesia_lib:unset({?MODULE,Tab}),
-      ok
-    catch _:_ ->
-	    ?DBG({double_delete, Tab}),
-	    ok
+        ets:delete(
+            mnesia_lib:val({?MODULE, Tab})),
+        mnesia_lib:unset({?MODULE, Tab}),
+        ok
+    catch
+        _:_ ->
+            ?DBG({double_delete, Tab}),
+            ok
     end.
 
-load_table(porset_copies, _Tab, init_index, _Cs) -> ok;
+load_table(porset_copies, _Tab, init_index, _Cs) ->
+    ok;
 load_table(porset_copies, _Tab, _LoadReason, _Cs) ->
     ?DBG("Load ~p ~p~n", [_Tab, _LoadReason]),
     ok.
+
 %%     mnesia_monitor:unsafe_create_external(Tab, porset_copies, ?MODULE, Cs).
 
 sender_init(Alias, Tab, _RemoteStorage, _Pid) ->
     KeysPerTransfer = 100,
     {standard,
-     fun() -> mnesia_lib:db_init_chunk({ext,Alias,?MODULE}, Tab, KeysPerTransfer) end,
-     fun(Cont) -> mnesia_lib:db_chunk({ext,Alias,?MODULE}, Cont) end}.
+     fun() -> mnesia_lib:db_init_chunk({ext, Alias, ?MODULE}, Tab, KeysPerTransfer) end,
+     fun(Cont) -> mnesia_lib:db_chunk({ext, Alias, ?MODULE}, Cont) end}.
 
 receiver_first_message(Sender, {first, Size}, _Alias, Tab) ->
-    ?DBG({first,Size}),
+    ?DBG({first, Size}),
     {Size, {Tab, Sender}}.
 
-receive_data(Data, porset_copies, Name, Sender, {Name, Tab, Sender}=State) ->
-    ?DBG({Data,State}),
+receive_data(Data, porset_copies, Name, Sender, {Name, Tab, Sender} = State) ->
+    ?DBG({Data, State}),
     true = ets:insert(Tab, Data),
     {more, State};
 receive_data(Data, Alias, Tab, Sender, {Name, Sender}) ->
-    receive_data(Data, Alias, Tab, Sender, {Name, mnesia_lib:val({?MODULE,Tab}), Sender}).
+    receive_data(Data, Alias, Tab, Sender, {Name, mnesia_lib:val({?MODULE, Tab}), Sender}).
 
 receive_done(_Alias, _Tab, _Sender, _State) ->
-    ?DBG({done,_State}),
+    ?DBG({done, _State}),
     ok.
 
-close_table(Alias, Tab) -> sync_close_table(Alias, Tab).
+close_table(Alias, Tab) ->
+    sync_close_table(Alias, Tab).
 
 sync_close_table(porset_copies, _Tab) ->
     ?DBG(_Tab).
 
 fixtable(porset_copies, Tab, Bool) ->
-    ?DBG({Tab,Bool}),
-    ets:safe_fixtable(mnesia_lib:val({?MODULE,Tab}), Bool).
+    ?DBG({Tab, Bool}),
+    ets:safe_fixtable(
+        mnesia_lib:val({?MODULE, Tab}), Bool).
 
 info(porset_copies, Tab, Type) ->
-    ?DBG({Tab,Type}),
-    Tid = mnesia_lib:val({?MODULE,Tab}),
+    ?DBG({Tab, Type}),
+    Tid = mnesia_lib:val({?MODULE, Tab}),
     try ets:info(Tid, Type) of
-	Val -> Val
-    catch _:_ ->
-	    undefined
+        Val ->
+            Val
+    catch
+        _:_ ->
+            undefined
     end.
 
 real_suffixes() ->
@@ -197,8 +195,11 @@ tmp_suffixes() ->
 
 %% Index
 
-index_is_consistent(_Alias, _Ix, _Bool) -> ok.  % Ignore for now
-is_index_consistent(_Alias, _Ix) -> false.      % Always rebuild
+index_is_consistent(_Alias, _Ix, _Bool) ->
+    ok.  % Ignore for now
+
+is_index_consistent(_Alias, _Ix) ->
+    false.      % Always rebuild
 
 %% Record operations
 
@@ -208,60 +209,75 @@ validate_record(_Alias, _Tab, RecName, Arity, Type, _Obj) ->
 validate_key(_Alias, _Tab, RecName, Arity, Type, _Key) ->
     {RecName, Arity, Type}.
 
-insert(Alias, Tab, Obj) ->
-    io:format("running my own lookup function~p~n", [Alias]),
-    ?DBG({Tab,Obj}),
+insert(Alias, Tab, {Obj, Ts}) ->
+    io:format("running my own insert function~p~n", [Alias]),
+    lager:debug({Tab, Obj}),
     try
-	ets:insert(mnesia_lib:val({?MODULE,Tab}), Obj),
-	ok
-    catch _:Reason ->
-	    io:format("CRASH ~p ~p~n",[Reason, mnesia_lib:val({?MODULE,Tab})])
+        ets:insert(
+            mnesia_lib:val({?MODULE, Tab}), {Obj, Ts, write}),
+        ok
+    catch
+        _:Reason ->
+            io:format("CRASH ~p ~p~n", [Reason, mnesia_lib:val({?MODULE, Tab})])
     end.
 
 lookup(porset_copies, Tab, Key) ->
     io:format("running my own lookup function~n"),
-    ets:lookup(mnesia_lib:val({?MODULE,Tab}), Key).
+    ets:lookup(
+        mnesia_lib:val({?MODULE, Tab}), Key).
 
 delete(porset_copies, Tab, Key) ->
-    ets:delete(mnesia_lib:val({?MODULE,Tab}), Key).
+    ets:delete(
+        mnesia_lib:val({?MODULE, Tab}), Key).
 
 match_delete(porset_copies, Tab, Pat) ->
-    ets:match_delete(mnesia_lib:val({?MODULE,Tab}), Pat).
+    ets:match_delete(
+        mnesia_lib:val({?MODULE, Tab}), Pat).
 
 first(porset_copies, Tab) ->
-    ets:first(mnesia_lib:val({?MODULE,Tab})).
+    ets:first(
+        mnesia_lib:val({?MODULE, Tab})).
 
-last(Alias, Tab) -> first(Alias, Tab).
+last(Alias, Tab) ->
+    first(Alias, Tab).
 
 next(porset_copies, Tab, Key) ->
-    ets:next(mnesia_lib:val({?MODULE,Tab}), Key).
+    ets:next(
+        mnesia_lib:val({?MODULE, Tab}), Key).
 
 prev(Alias, Tab, Key) ->
     next(Alias, Tab, Key).
 
 slot(porset_copies, Tab, Pos) ->
-    ets:slot(mnesia_lib:val({?MODULE,Tab}), Pos).
+    ets:slot(
+        mnesia_lib:val({?MODULE, Tab}), Pos).
 
 update_counter(porset_copies, Tab, C, Val) ->
-    ets:update_counter(mnesia_lib:val({?MODULE,Tab}), C, Val).
+    ets:update_counter(
+        mnesia_lib:val({?MODULE, Tab}), C, Val).
 
-select('$end_of_table' = End) -> End;
-select({porset_copies, C}) ->  ets:select(C).
+select('$end_of_table' = End) ->
+    End;
+select({porset_copies, C}) ->
+    ets:select(C).
 
 select(Alias, Tab, Ms) ->
     Res = select(Alias, Tab, Ms, 100000),
     select_1(Res).
 
-select_1('$end_of_table') -> [];
+select_1('$end_of_table') ->
+    [];
 select_1({Acc, C}) ->
     case ets:select(C) of
-	'$end_of_table' -> Acc;
-	{New, Cont} ->
-	    select_1({New ++ Acc, Cont})
+        '$end_of_table' ->
+            Acc;
+        {New, Cont} ->
+            select_1({New ++ Acc, Cont})
     end.
 
 select(porset_copies, Tab, Ms, Limit) when is_integer(Limit); Limit =:= infinity ->
-    ets:select(mnesia_lib:val({?MODULE,Tab}), Ms, Limit).
+    ets:select(
+        mnesia_lib:val({?MODULE, Tab}), Ms, Limit).
 
 repair_continuation(Cont, Ms) ->
     ets:repair_continuation(Cont, Ms).
