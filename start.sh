269885e9fc0f1fa09e0ede86c17800bb087cf97c
Shuntian Liu <shuntian.liu@outlook.com>
2023-10-07 21:49:57 +0100

    remove eflame

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-
-~/Developer/otp/bin/erl -mnesia dir '"priv/'$1'"' debug debug -name $1@127.0.0.1 -pa \
+# add debug debug after -mnesia dir '"priv/'$1'"' to enable mnesia debug
+~/Developer/otp/bin/erl -mnesia dir '"priv/'$1'"' -name $1@127.0.0.1 -pa \
 `rebar3 path` -pa '/Users/vincent/Developer/inet_tcp_proxy/_build/default/lib/inet_tcp_proxy_dist/ebin' \
 -setcookie cookie $2 $3
 
